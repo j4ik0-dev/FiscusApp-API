a90f1b050b52d5b5dbcad54ea3c159abe06f0295
j4ik0 <salazzarjefferson@gmail.com>
2026-01-08 21:05:03 -0600

    se programo los metodos para la api y se agrego a la ruta api

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Expense;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ExpenseController extends Controller
 {
@@ -12,7 +14,8 @@ class ExpenseController extends Controller
      */
     public function index()
     {
-        //
+        $expenses = Expense::orderBy('created_at', 'desc')->get();
+        return response()->json($expenses);
     }
 
     /**
@@ -20,7 +23,32 @@ class ExpenseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // 1. Validamos que los datos vengan bien
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'amount' => 'required|numeric',
+            'type' => 'required|in:fixed,variable,ant', // Solo acepta lo que definimos
+            'receipt' => 'nullable|image|max:2048', // Máximo 2MB por foto
+            // Por ahora enviaremos el user_id manual hasta que hagamos el Login
+            'user_id' => 'required|exists:users,id' 
+        ]);
+
+        $data = $request->all();
+
+        // 2. Lógica de la Foto (Recibo)
+        if ($request->hasFile('receipt')) {
+            // Guarda en la carpeta 'public/receipts' y nos da la ruta única
+            $path = $request->file('receipt')->store('receipts', 'public');
+            $data['receipt_path'] = $path;
+        }
+
+        // 3. Crear el gasto en BD
+        $expense = Expense::create($data);
+
+        return response()->json([
+            'message' => 'Gasto guardado correctamente',
+            'data' => $expense
+        ], 201);
     }
 
     /**
