e365e58c7b7044213cd5ec944e80d6e522a82fd1
j4ik0 <salazzarjefferson@gmail.com>
2026-01-19 16:42:16 -0600

    se agregaron rutas publicas y rutas protegidas

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExpenseController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::apiResource('expenses', ExpenseController::class);
+//RUTAS PÚBLICAS
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//RUTAS PROTEGIDAS
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Aquí metemos tus rutas de gastos para que solo el dueño pueda verlas
+    Route::apiResource('expenses', ExpenseController::class);
+    // Ruta para obtener mis datos
+    Route::get('/user', function (Illuminate\Http\Request $request) {
+        return $request->user();
+    });
+});
